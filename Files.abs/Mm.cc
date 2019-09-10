6d9d91f973e33839e79c5c3d69075e97503f2785
gmright1 <54698352+gmright1@users.noreply.github.com>
2019-09-10 00:37:00 -0500

    Create Mm.cc

---
+++ Files.abs/Mm.cc
@@ -0,0 +1,1 @@
+Sooo Paul and I got back from Nebraska last night about midnight. And Paul just got home from work and needs to pack to leave for out of town immediately after work tomorrow for a week. Soooo we need to unpack so he can then repack to leave tomorrow. So we won't be there tonight, but I will plan to be there next Monday. But Paul won't be back yet next week either.
